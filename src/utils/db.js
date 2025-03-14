c9d9f24667925a8ea6849353143bcafb9dca9446
Koppula Ravi <ravikoppula92@gmail.com>
2025-03-14 10:21:00 -0400

    changes in db.js file

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,17 +2,25 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 const connectDB = async () => {
+    
     try {
+
         await mongoose.connect(process.env.MONGO_URI, {
+
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
             useFindAndModify: false
+
         });
+
         logger.info('MongoDB connected successfully');
+
     } catch (error) {
+
         logger.error('MongoDB connection failed', error);
         process.exit(1);
+
     }
 };
 
