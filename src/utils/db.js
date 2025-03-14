c80c392ad27e771df814df373782ac7507092065
Koppula Ravi <ravikoppula92@gmail.com>
2025-03-14 10:48:26 -0400

    integrated with swagger api

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 const connectDB = async () => {
-    
+
     try {
 
         await mongoose.connect(process.env.MONGO_URI, {
 
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+
+            //useCreateIndex: true,
+            //useFindAndModify: false
 
         });
 
